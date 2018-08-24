30a1e844678545ed3867704f2ce6427c951981be
Y3ll0wman <varvarkin_89@mail.ru>
2018-08-24 22:53:13 +0300

    full flex,footer optimized

--- js/index-nav.js
+++ js/index-nav.js
@@ -6,6 +6,8 @@ $( document ).ready(function() {
         $(".navigation__metalStructures").css("box-shadow", "0px -4px 3px 2px rgba(0,0,0,.1)");
         $(".navigation__tablets").css("background-color", "transparent");
         $(".navigation__tablets").css("box-shadow", "0px 0px 0px 0px rgba(0,0,0,.1)");
+        $(".navigation__tablets__line").css("display", "none");
+        $(".navigation__metalStructures__line").css("display", "block");
     });
     $('#navigation__tablets').click(function(){
         $(".metalStructuresCards").css("display", "none");
@@ -14,6 +16,8 @@ $( document ).ready(function() {
         $(".navigation__tablets").css("box-shadow", "0px -4px 3px 2px rgba(0,0,0,.1)");
         $(".navigation__metalStructures").css("background-color", "transparent");
         $(".navigation__metalStructures").css("box-shadow", "0px 0px 0px 0px rgba(0,0,0,.1)");
+        $(".navigation__tablets__line").css("display", "block");
+        $(".navigation__metalStructures__line").css("display", "none");
     });
     $('.tabletsCards__item1__icon').mouseover(function(){
         $(".tabletsCards__icon__effect1").css("display", "flex");
